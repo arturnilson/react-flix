c2ab144b0c12bfbda6ffc2d11d81b8b5b4271e94
Artur Nilson <arturnilson@gmail.com>
2020-07-29 00:25:49 -0300

    Ajustes e Implementação de parte da Home

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const CadastroVideo = () => {
+  return <div>Cadastro de Videos</div>
+}
+
+const Pagina404 = () => (<div>Página 404</div>)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
