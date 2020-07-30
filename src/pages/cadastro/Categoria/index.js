6d60e04dd37792cd239a514e4f98186a9cbb8973
Artur Nilson <arturnilson@gmail.com>
2020-07-29 23:58:53 -0300

    Commit

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
 
+    const [nomeCategoria, setNomeCategoria] = useState('Filmes');
+
     return (
         <PageDefault>
-            <h1>Cadastro Categoria</h1>
+            <h1>Cadastro Categoria: {nomeCategoria}</h1>
+
+            <form>
+                <label>Nome da Categoria</label>
+
+                <input
+                    type="text"
+                    value={nomeCategoria}
+                    onChange={function funcaoOnChange(info) {
+                        console.log('[nomeCategoria]', nomeCategoria);
+                        console.log('[info]', info);
+                    }}
+                />
+                <button>Cadastrar</button>
+            </form>
+
 
             <Link to="/">
                 Ir para Home
